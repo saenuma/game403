0257a4a33062d810d5bc390ad26305238d27dee8
Bankole Ojo <ojobankole@gmail.com>
2024-04-03 23:19:59 +0100

    first set of assets

---
+++ g403/export.sh
@@ -0,0 +1,5 @@
+inkscape --export-type="png" 1.svg
+inkscape --export-type="png" 2.svg
+inkscape --export-type="png" 3.svg
+inkscape --export-type="png" 4.svg
+inkscape --export-type="png" 5.svg
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/saenuma/Game403
+
+go 1.22.1
