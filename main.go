a25310bdff22481ce4cf72d5c11d109258d04ae1
Bankole Ojo <ojobankole@gmail.com>
2024-04-04 00:00:11 +0100

    basic game implemented
    with only five scenes

--- main.go
+++ main.go
@@ -19,9 +19,13 @@ const (
 	fps        = 10
 	sceneLimit = 5
 	fontSize   = 30
+
+	BlessBtn = 101
+	CurseBtn = 102
 )
 
 var objCoords map[int]g143.RectSpecs
+var currentScene int
 
 func main() {
 	runtime.LockOSThread()
@@ -29,10 +33,11 @@ func main() {
 	objCoords = make(map[int]g143.RectSpecs)
 
 	window := g143.NewWindow(1200, 800, "Game403: a game about rewards", false)
-	allDraws(window, 1)
+	currentScene = 1
+	drawScene(window, 1)
 
 	// respond to the mouse
-	// window.SetMouseButtonCallback(mouseBtnCallback)
+	window.SetMouseButtonCallback(mouseBtnCallback)
 
 	for !window.ShouldClose() {
 		t := time.Now()
@@ -49,7 +54,7 @@ func getDefaultFontPath() string {
 	return fontPath
 }
 
-func allDraws(window *glfw.Window, scene int) {
+func drawScene(window *glfw.Window, scene int) {
 	wWidth, wHeight := window.GetSize()
 
 	// frame buffer
@@ -74,10 +79,6 @@ func allDraws(window *glfw.Window, scene int) {
 	ggCtx.SetHexColor("#444")
 	ggCtx.DrawString(msg, float64(msgX), 10+fontSize)
 
-	if scene > sceneLimit {
-		scene = 1
-	}
-
 	rawPNG, err := PNGs.ReadFile(fmt.Sprintf("pngs/%d.png", scene))
 	if err != nil {
 		panic(err)
@@ -93,7 +94,6 @@ func allDraws(window *glfw.Window, scene int) {
 	ggCtx.DrawImage(img, imgX, 60)
 
 	// buttons
-
 	msgR1, msgR2 := "BLESS", "CURSE"
 	msgR1W, _ := ggCtx.MeasureString(msgR1)
 	msgR2W, _ := ggCtx.MeasureString(msgR2)
@@ -102,6 +102,8 @@ func allDraws(window *glfw.Window, scene int) {
 	ggCtx.SetHexColor("#8B5A87")
 	ggCtx.DrawRoundedRectangle(200, float64(buttonsY), msgR1W+40, 50, 10)
 	ggCtx.Fill()
+	bbRS := g143.NRectSpecs(200, buttonsY, int(msgR1W+40), 50)
+	objCoords[BlessBtn] = bbRS
 
 	ggCtx.SetHexColor("#fff")
 	ggCtx.DrawString(msgR1, 200+20, float64(buttonsY)+fontSize+5)
@@ -109,6 +111,8 @@ func allDraws(window *glfw.Window, scene int) {
 	ggCtx.SetHexColor("#85836E")
 	ggCtx.DrawRoundedRectangle(900, float64(buttonsY), msgR2W+40, 50, 10)
 	ggCtx.Fill()
+	cbRS := g143.NRectSpecs(900, buttonsY, int(msgR2W)+40, 50)
+	objCoords[CurseBtn] = cbRS
 
 	ggCtx.SetHexColor("#fff")
 	ggCtx.DrawString(msgR2, 900+20, float64(buttonsY)+fontSize+5)
@@ -118,4 +122,44 @@ func allDraws(window *glfw.Window, scene int) {
 	g143.DrawImage(wWidth, wHeight, ggCtx.Image(), windowRS)
 	window.SwapBuffers()
 
+}
+
+func mouseBtnCallback(window *glfw.Window, button glfw.MouseButton, action glfw.Action, mods glfw.ModifierKey) {
+	if action != glfw.Release {
+		return
+	}
+
+	xPos, yPos := window.GetCursorPos()
+	xPosInt := int(xPos)
+	yPosInt := int(yPos)
+
+	// wWidth, wHeight := window.GetSize()
+
+	// var widgetRS g143.RectSpecs
+	var widgetCode int
+
+	for code, RS := range objCoords {
+		if g143.InRectSpecs(RS, xPosInt, yPosInt) {
+			// widgetRS = RS
+			widgetCode = code
+			break
+		}
+	}
+
+	if widgetCode == 0 {
+		return
+	}
+
+	currentScene += 1
+	if currentScene > sceneLimit {
+		currentScene = 1
+	}
+
+	switch widgetCode {
+	case BlessBtn:
+		drawScene(window, currentScene)
+
+	case CurseBtn:
+		drawScene(window, currentScene)
+	}
 }
