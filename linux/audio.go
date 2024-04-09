cb556712cbae463125c99c8277a346f6062811ae
Bankole Ojo <ojobankole@gmail.com>
2024-04-09 22:29:54 +0100

    added a missing package to snapcraft config

--- linux/audio.go
+++ linux/audio.go
@@ -27,9 +27,9 @@ func playAudio() {
 func GetMPGCommand() string {
 	var cmdPath string
 	begin := os.Getenv("SNAP")
-	cmdPath = "mpg321"
+	cmdPath = "madplay"
 	if begin != "" && !strings.HasPrefix(begin, "/snap/go/") {
-		cmdPath = filepath.Join(begin, "usr", "bin", "mpg321")
+		cmdPath = filepath.Join(begin, "usr", "bin", "madplay")
 	}
 
 	return cmdPath
