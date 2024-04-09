34af4f527a44d80d83ecf61e35bf2895ba139b44
Bankole Ojo <ojobankole@gmail.com>
2024-04-09 21:51:58 +0100

    fixing the last commit

--- linux/audio.go
+++ linux/audio.go
@@ -5,14 +5,28 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 )
 
 func playAudio() {
 	audioPath := filepath.Join(os.TempDir(), "audio.mp3")
 	os.WriteFile(audioPath, AudioBytes, 0666)
 
+	mpg := GetMPGCommand()
+
 	ctx, cancel := context.WithCancel(context.Background())
 	linuxCancelFn = cancel
-	cmd := exec.CommandContext(ctx, "mpg321", audioPath)
+	cmd := exec.CommandContext(ctx, mpg, audioPath)
 	cmd.Run()
+}
+
+func GetMPGCommand() string {
+	var cmdPath string
+	begin := os.Getenv("SNAP")
+	cmdPath = "mpg321"
+	if begin != "" && !strings.HasPrefix(begin, "/snap/go/") {
+		cmdPath = filepath.Join(begin, "usr", "bin", "mpg321")
+	}
+
+	return cmdPath
 }
